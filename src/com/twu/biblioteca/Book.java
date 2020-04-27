e40f842d7785209a86956699faef63ce4956dd69
Jiayujessie <Jiayujessie@users.noreply.github.com>
2020-04-27 22:45:22 +0800

    1.2 / View a list of all books

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+
+public class Book {
+    String name;
+    String author;
+    String publishYear;
+
+
+
+    public String seeBookList() {
+        return name;
+    }
+
+
+}
